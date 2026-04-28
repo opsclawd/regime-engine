c10f360eb542f2325b1f9ee386f6faab469b5c12
opsclawd <opsclawd@gmail.com>
2026-04-28 11:50:53 -0600

    m022: add createDb factory with search_path=regime_engine

---
+++ src/ledger/pg/db.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+export function createDb(connectionString: string): {
+  db: ReturnType<typeof drizzle>;
+  client: ReturnType<typeof postgres>;
+} {
+  const client = postgres(connectionString);
+
+  const db = drizzle(client);
+
+  return { db, client };
+}
+
+export type Db = ReturnType<typeof createDb>["db"];
